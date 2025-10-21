c9e0996543e7577afd67d897ea2419599fbc9413
Thank-05 <thanhtiennguyen4805@gmail.com>
2025-10-21 22:19:09 +0700

    updaate21/10

---
+++ Web/complete/src/main/java/com/example/servingwebcontent/Database/myConnection.java
@@ -0,0 +1,33 @@
+package com.example.servingwebcontent.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+
+@Controller
+public class myConnection {
+    @Value("${app.database.url}")
+    private String urlString;
+
+    @Value("${app.database.driver}")
+    private String appDriver;
+
+public Statement myDbConnection(){
+    Connection conn = null;
+        try {
+           
+            Class.forName(appDriver);
+            conn = DriverManager.getConnection(
+                    urlString);
+            Statement sta = conn.createStatement();
+            return sta;
+}
+
+catch(Exception e){
+    System.out.println(e);
+}
+
+return null;
+} 
+}
